484ebc1cf75a68176b1c4849320ba232a02caf76
AlexisFAP <alexispatinoagudelo@gmail.com>
2022-12-18 01:18:44 -0500

    About page almost done (need to implement cv download)

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -5,20 +5,30 @@ import './about.css'
 
 function About() {
     return (
-        <div>
+        <div className="about">
             <div className="about-title">About me</div>
             <div className="about-container">
                 <div className="subcontainer">
                     <div className="subtitle">Fullstack Developer / Frontend - Backend</div>
-                    <div className="text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                        when an unknown printer took a galley of type and scrambled it to make a type 
-                        specimen book.</div>
-                    <button className="btn">Download CV</button>
+                    <div className="text">Hi, i'm Alexis a System Engineering Student of the University of Medellín,
+                        i like building Web Apps and learning new technologies because of the satisfaction i get by overcoming challenges.
+                    </div>
+                    <button className="btn"><img src="file-download.png" alt="file-download.png" /> Download CV</button>
                 </div>
                 <div className="subcontainer">
                     <div className="subtitle">Skills & Technologies</div>
-                    <h2>HERE ICONS</h2>
+                    <div className="about-img-container">
+                        <div><img className="about-img" src="js.png" alt="js.png" /><div className="text"> Javascript</div></div>
+                        <div><img className="about-img" src="css.png" alt="css.png" /><div className="text"> Css</div></div>
+                        <div><img className="about-img" src="html.png" alt="html.png" /><div className="text"> Html</div></div>
+                        <div><img className="about-img" src="react.png" alt="react.png" /><div className="text"> React</div></div>
+                        <div><img className="about-img" src="nuxt.png" alt="nuxt.png" /><div className="text"> Nuxt.js</div></div>
+                        <div><img className="about-img" src="node.png" alt="node.png" /><div className="text"> Node.js</div></div>
+                        <div><img className="about-img" src="express.png" alt="express.png" /><div className="text"> Express.js</div></div>
+                        <div><img className="about-img" src="mongo.png" alt="mongo.png" /><div className="text"> MongoDB</div></div>
+                        <div><img className="about-img" src="postgres.png" alt="postgres.png" /><div className="text"> PostgreSQL</div></div>
+                        <div><img className="about-img" src="git.png" alt="git.png" /><div className="text"> Git</div></div>
+                    </div>
                 </div>
             </div>
         </div>
